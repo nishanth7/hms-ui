443228837b65b1d7290b8ec57e86c2e1a5fbdf76
nishanth <n.ganesh@everteam.com>
2024-12-04 14:58:25 +0530

    Add vercel reverse proxy with heroku.

--- proxy.config.js
+++ proxy.config.js
@@ -2,7 +2,7 @@
 
 const PROXY_CONFIG = {
   '/v1/**': {
-    target: 'http://localhost:3000',
+    target: 'https://b7ac-49-207-214-186.ngrok-free.app',
     changeOrigin: true,
     secure: false,
     logLevel: 'debug',
